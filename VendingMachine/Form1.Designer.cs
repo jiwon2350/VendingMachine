2ca996038c8a0fbda3de3b14a912df6e4cc2af3b
Hong Jiwon <145486796+jiwon2350@users.noreply.github.com>
2024-12-31 12:19:08 +0900

    초기 커밋.

---
+++ VendingMachine/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace VendingMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_PlayerHP = new System.Windows.Forms.TextBox();
+            this.textBox_MonsterHP = new System.Windows.Forms.TextBox();
+            this.button_fight = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_PlayerHP
+            // 
+            this.textBox_PlayerHP.Location = new System.Drawing.Point(43, 95);
+            this.textBox_PlayerHP.Multiline = true;
+            this.textBox_PlayerHP.Name = "textBox_PlayerHP";
+            this.textBox_PlayerHP.Size = new System.Drawing.Size(273, 197);
+            this.textBox_PlayerHP.TabIndex = 0;
+            // 
+            // textBox_MonsterHP
+            // 
+            this.textBox_MonsterHP.Location = new System.Drawing.Point(338, 95);
+            this.textBox_MonsterHP.Multiline = true;
+            this.textBox_MonsterHP.Name = "textBox_MonsterHP";
+            this.textBox_MonsterHP.Size = new System.Drawing.Size(269, 197);
+            this.textBox_MonsterHP.TabIndex = 1;
+            // 
+            // button_fight
+            // 
+            this.button_fight.Location = new System.Drawing.Point(629, 244);
+            this.button_fight.Name = "button_fight";
+            this.button_fight.Size = new System.Drawing.Size(106, 48);
+            this.button_fight.TabIndex = 2;
+            this.button_fight.Text = "Fight!!";
+            this.button_fight.UseVisualStyleBackColor = true;
+            this.button_fight.Click += new System.EventHandler(this.button_fight_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Monster";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Player";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_fight);
+            this.Controls.Add(this.textBox_MonsterHP);
+            this.Controls.Add(this.textBox_PlayerHP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_PlayerHP;
+        private System.Windows.Forms.TextBox textBox_MonsterHP;
+        private System.Windows.Forms.Button button_fight;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ VendingMachine/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VendingMachine
+{
+    public partial class Form1 : Form
+    {
+        private Player player; // 전역 플레이어 
+        private Monster Goblin; // 전역 몬스터 
+        private Monster Slime;//전역 슬라임
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 플레이어와 몬스터 초기화
+            this.player = new Player { Name = "Hero", Health = 300, AttackPower = 18 };
+            this.Goblin = new Monster { Name = "Goblin", Health = 100, AttackPower = 33 };
+            this.Slime = new Monster { Name = "Slime", Health = 200, AttackPower = 27 };        
+        }
+
+        private void SimulateBattle()
+        {
+            // 전투 시작 메시지
+            MessageBox.Show("전투 시작!");
+
+            // 전투 루프
+            while (player.Health > 0 && Goblin.Health > 0)
+            {
+                // 플레이어가 몬스터 선공
+                player.Attack(Goblin);
+                textBox_MonsterHP.Text += "남은 HP = " + Goblin.Health.ToString()+ "\r\n";
+                Goblin.Attack(player);
+                textBox_PlayerHP.Text += "남은 HP = " + player.Health.ToString()+ "\r\n";
+                // 몬스터의 2번째 공격은 분노 공격
+                player.Attack(Goblin);
+                textBox_MonsterHP.Text += "남은 HP = " + Goblin.Health.ToString() + "\r\n";
+                Goblin.RageAttack(player);
+                textBox_PlayerHP.Text += "남은 HP = " + player.Health.ToString() + "\r\n";
+
+
+            }
+
+            // 전투 종료 메시지
+            if (player.Health > 0 && Goblin.Health <= 0)
+            {
+                MessageBox.Show("플레이어가 승리했습니다!");
+            }
+            else if (Goblin.Health > 0 && player.Health <= 0)
+            {
+                MessageBox.Show("몬스터가 승리했습니다!");
+            }
+            else
+            {
+                MessageBox.Show("둘 다 살아남았습니다. 무승부입니다.");
+            }
+        }
+
+        private void button_fight_Click(object sender, EventArgs e)
+        {
+            // 전투시작 버튼 
+            SimulateBattle();
+        }
+        
+
+    }
+
+    // 캐릭터 기본 클래스
+    public abstract class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public abstract void Attack(Character target);
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0) Health = 0;
+            MessageBox.Show($"{Name}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {Health}");
+        }
+    }
+
+    // 플레이어 클래스
+    public class Player : Character
+    {
+        public override void Attack(Character target)
+        {
+            MessageBox.Show($"{Name}이(가) {target.Name}을(를) 공격했습니다!");
+            target.TakeDamage(AttackPower);
+        }
+    }
+
+    // 몬스터 클래스
+    public class Monster : Character
+    {
+        public override void Attack(Character target)
+        {
+            MessageBox.Show($"{Name}이(가) {target.Name}을(를) 공격했습니다!");
+            target.TakeDamage(AttackPower);
+        }
+
+        // 분노 공격 메서드
+        public void RageAttack(Character target)
+        {
+            int rageDamage = AttackPower * 3; // rage 모드에서의 공격력 3배 
+            target.TakeDamage(rageDamage);
+            MessageBox.Show($"{Name}이(가) 분노 공격으로 {target.Name}에게 {rageDamage} 데미지를 입혔습니다!");
+        }
+    }
+
+}
